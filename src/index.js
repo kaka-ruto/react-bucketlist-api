475e50a4ce199beb7c8903a31bf5d61566059890
borenho <borecollins@gmail.com>
2017-09-16 12:43:53 +0300

    Create a login page using material-ui

--- src/index.js
+++ src/index.js
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Login from './Login';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Appp = () => (
+    <MuiThemeProvider>
+        <Login />
+    </MuiThemeProvider>
+);
+
+ReactDOM.render(<Appp />, document.getElementById('root'));
 registerServiceWorker();
 
 // Hot Module Reloading - reloads the appplication and not the page
